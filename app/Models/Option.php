be282efd4305d65433d4e50821d496a7da571d5a
doba nabil <dobanabil40@gmail.com>
2020-10-10 02:32:57 +0200

    Update Option.php

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    protected $table = 'options';
     protected $fillable = [
         'face', 'insta','whats' ,'phone' , 'active' , 'ios' , 'andriod','created_at' , 'updated_at'
     ];
