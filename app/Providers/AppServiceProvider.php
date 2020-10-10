d94318a7aaab0380eb64149204c1a949293257c3
doba nabil <dobanabil40@gmail.com>
2020-10-10 02:25:42 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,18 +26,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-//         $option = Option::find(1);
-//         if(!isset($option)){
-//             $option = new Option();
-//             $option->id = 1;
-//             $option->face = 'facebook.com';
-//             $option->insta = 'instagram.com';
-//             $option->phone = '99999900000000';
-//             $option->whats = '99999900000000';
-//             $option->ios = 'https://apps.apple.com/';
-//             $option->andriod = 'https://play.google.com/';
-//             $option->active = 1;
-//             $option->save();
-//         }
+        if(\Schema::hasTable('options')){
+            $option = Option::find(1);
+            if(!isset($option)){
+                $option = new Option();
+                $option->id = 1;
+                $option->face = 'facebook.com';
+                $option->insta = 'instagram.com';
+                $option->phone = '99999900000000';
+                $option->whats = '99999900000000';
+                $option->ios = 'https://apps.apple.com/';
+                $option->andriod = 'https://play.google.com/';
+                $option->active = 1;
+                $option->save();
+            }
+        }
     }
 }
