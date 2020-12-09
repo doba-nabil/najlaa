63bf8db569a6d6d761bd8336194eaf5eddf73de2
doba nabil <dobanabil40@gmail.com>
2020-12-09 02:59:51 +0200

    clear cache

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,21 @@ Route::group(['namespace' => 'Admin', 'middleware' => 'auth:moderator'], functio
     Route::get('register', 'AdminauthController@showLoginFrom')->name('backendLogin');
 });
 
+//Clear config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return 'Config cache cleared';
+});
+
+// Clear application cache:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return 'View cache cleared';
+});
+
