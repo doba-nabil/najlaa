e5d46dd4a7509e36b476901cc1b8451e404470d7
unknown <dobanabil40@gmail.com>
2021-05-06 01:29:07 +0200

    delete tokens edit 22

--- app/Http/Middleware/localization.php
+++ app/Http/Middleware/localization.php
@@ -18,35 +18,39 @@ class localization
         $local = ($request->hasHeader('lang')) ? $request->header('lang') : 'en';
         // set laravel localization
         app()->setLocale($local);
-        $token = \Request::header('token');
-        if($request->bearerToken()){
-            $user = User::where('api_token', $request->bearerToken())->first();
-            $user_token = DB::table('token_users')->where('user_id' , $user->id)->where('device_token' , $token)->first();
-            if(!isset($user_token)){
-                DB::table('token_users')->insert(
-                    array(
-                        'user_id'     =>   $user->id,
-                        'device_token'   =>  $token
-                    )
-                );
-            }
-        }else{
-            if(isset($token)){
-                $user_token = DB::table('token_users')->where('device_token' , $token)->first();
+        if(\Request::is('api/login') || \Request::is('api/register')){
+            $token = \Request::header('token');
+
+            if($request->bearerToken()){
+                $user = User::where('api_token', $request->bearerToken())->first();
+                $user_token = DB::table('token_users')->where('user_id' , $user->id)->where('device_token' , $token)->first();
                 if(!isset($user_token)){
                     DB::table('token_users')->insert(
                         array(
-                            'device_token'   =>  $token,
-                            'lang'   =>  $local
+                            'user_id'     =>   $user->id,
+                            'device_token'   =>  $token
                         )
                     );
-                }elseif(isset($user_token)){
-                    DB::table('token_users')->where('device_token' , $token)->update(array(
-                        'lang'=>$local,
-                    ));
+                }
+            }else{
+                if(isset($token)){
+                    $user_token = DB::table('token_users')->where('device_token' , $token)->first();
+                    if(!isset($user_token)){
+                        DB::table('token_users')->insert(
+                            array(
+                                'device_token'   =>  $token,
+                                'lang'   =>  $local
+                            )
+                        );
+                    }elseif(isset($user_token)){
+                        DB::table('token_users')->where('device_token' , $token)->update(array(
+                            'lang'=>$local,
+                        ));
+                    }
                 }
             }
         }
+
         // continue request
         return $next($request);
     }
